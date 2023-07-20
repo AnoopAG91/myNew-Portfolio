aa684240cd958fb371528a7cba2b5d85bc2809a7
AnoopAG91 <anoopag47@gmail.com>
2023-07-20 21:03:13 +0530

    hero section added in home

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import './home.css'
+import Typed from 'react-typed';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='hero-wrapper'>
+        <div className='main-info'>
+            <h1>Hi, I'm ANOOP.</h1>
+            <h3>-Front-End Developer-</h3>
+            
+            <Typed
+                className='typed-text'
+                strings={["Web Design", "Web Development", "Facebook Ads", "Google Ads"]}
+                typeSpeed={80}
+                backSpeed={100}
+                loop
+            />
+           
+            <button className='main-btn'>Contact Me</button>
+        </div>
+    </div>
   )
 }
 
