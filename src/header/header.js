dfb5c4eb02b84a9a68a19dd2eecbd42131307c2a
AnoopAG91 <anoopag47@gmail.com>
2023-07-20 20:55:31 +0530

    my new portfolio

---
+++ src/header/header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './header.css'
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import {BrowserRouter,Routes, Route,Link} from 'react-router-dom'
+import Home from '../pages/home';
+import About from '../pages/about'
+import Projects from '../pages/projects';
+
+function Header() {
+  return (
+    <BrowserRouter>
+    <div className='Header'>
+    <Navbar expand="lg" className="bg-dark">
+      <Container>
+        <Navbar.Brand as={Link} to={"/"}>Logo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+            <Nav.Link as={Link} to={"/about"}>About</Nav.Link>
+            <Nav.Link as={Link} to={"/projects"}>projects</Nav.Link>
+            <Nav.Link as={Link} to={"/contact"}>contact</Nav.Link>
+           
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+     
+</div>
+</BrowserRouter>
+
+  )
+
+}
+
+export default Header
